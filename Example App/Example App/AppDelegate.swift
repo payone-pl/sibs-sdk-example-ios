7a2f114a0b0e9fbced151d06fdb19e2967acb5f7
Marcin Kuświk <46490752+marcin-kuswik@users.noreply.github.com>
2022-12-31 17:58:49 +0100

    Release 1.0.2 (#2)

--- Example App/Example App/AppDelegate.swift
+++ Example App/Example App/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        SIBS.SDK.configure(withBaseURL: baseURL, webURL: webURL, clientID: clientID, token: token, language: .en)
+        SIBS.configure(withBaseURL: baseURL, webURL: webURL, clientID: clientID, token: token, language: .en)
         return true
     }
 
